994912a5d86fc233ee23f31b12fd052392604d41
Shivanker Goel <shivanker.goel@gmail.com>
2015-03-05 22:59:56 +0530

    Completed msort for MPI

--- msort.cpp
+++ msort.cpp
@@ -60,20 +60,37 @@ void mSort(dataType *data, int n_total)    {
     MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
     int n = n_total;
 
-    // TODO: Send data to everyone
     // allocate the extra buffer merge sort needs
     dataType *res = new dataType[n];
     omp_set_num_threads(omp_get_num_procs()*4);
 
-    // sort your segment using omp quick sort
-    int a = mpi_seg_start(0, n, mpirank, mpisize),
-        b = mpi_seg_end  (0, n, mpirank, mpisize);
+    const int a = mpi_seg_start(0, n, mpirank, mpisize);
+    int       b = mpi_seg_end  (0, n, mpirank, mpisize);
+
+    // send data to all nodes
+    if(mpirank == 0)    {
+        for(int i = 1; i < mpisize; ++i)    {
+            int start = mpi_seg_start(0, n, i, mpisize),
+                end   = mpi_seg_end  (0, n, i, mpisize);
+            if(start < end)
+                MPI_Send(data+start, end-start, dataType_MPI(), i,
+                        8192+i, MPI_COMM_WORLD);
+        }
+    }
+    else    {
+        if(a < b)
+            MPI_Recv(data+a, b-a, dataType_MPI(), 0,
+                    8192+mpirank, MPI_COMM_WORLD, &status);
+    }
+
+    // sort only your segment using omp quick sort
     n = b-a;
     data = data+a;
     res = res+a;
     #pragma omp parallel
     {
         int i;
+
         #pragma omp for
         for (i = 0; i < n; ++i)
             res[i] = data[i];
@@ -88,25 +105,22 @@ void mSort(dataType *data, int n_total)    {
     res = res-a;
     n = n_total;
 
-    MPI_Barrier(MPI_COMM_WORLD);
     // now we have a sorted segment with each node.
-    int factor = 2;
+    int factor = 1;
     bool active = true;
     do {
-        if(mpirank == 0)
-            printf("factor: %d\n", factor);
-        if(active && mpirank % factor != 0) {
+        factor *= 2;
+        if(mpirank % factor != 0) {
             // hand over data to parent, and deactivate
             int parent = mpirank - (mpirank % factor);
             MPI_Send(&b, 1, MPI_INT, parent, factor+4096, MPI_COMM_WORLD);
             if(a < b)   {
-                printf("%d sending %d to %d.\n", mpirank, a, b);
                 MPI_Send(data+a, b-a, dataType_MPI(), parent,
                             factor, MPI_COMM_WORLD);
             }
             active = false;
         }
-        else if(active && mpirank+factor/2 < mpisize) {
+        else if(mpirank+factor/2 < mpisize) {
             // get data from all child, and merge
             int temp;
             MPI_Recv(&temp, 1, MPI_INT, MPI_ANY_SOURCE,
@@ -114,8 +128,7 @@ void mSort(dataType *data, int n_total)    {
             if(b < temp)    {
                 MPI_Recv(data+b, temp-b, dataType_MPI(), MPI_ANY_SOURCE,
                             factor, MPI_COMM_WORLD, &status);
-                printf("%d got %d to %d", mpirank, b, temp);
-                merge(data+a, b-a, temp-b, res+a);
+                merge(data+a, b-a, temp-a, res+a);
                 b = temp;
 
                 // copy back from res to data
@@ -123,11 +136,8 @@ void mSort(dataType *data, int n_total)    {
                 for(int i = a; i < b; ++i)
                     data[i] = res[i];
             }
-
         }
-        factor *= 2;
-        MPI_Barrier(MPI_COMM_WORLD);
-    } while(divceil(mpisize, factor) > 1);
+    } while(active && divceil(mpisize, factor) > 1);
 
     delete [] res;
 }
