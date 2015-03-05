91020af2539a191007c1e7092493eff8015df058
Shivanker Goel <shivanker.goel@gmail.com>
2015-03-05 22:39:37 +0530

    Don't distribute data in test,check. Better error handling in check.

--- check.cpp
+++ check.cpp
@@ -7,51 +7,37 @@ using namespace std;
 
 int main(int argc, char **argv)  {
     MPI_Init(&argc, &argv);
-    MPI_Status status;
     int mpirank, mpisize;
     MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
     MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
 
-    const int n = 32;
+    const int n = 1<<17;
     int i;
     bool failed = false;
-    int a = mpi_seg_start(0, n, mpirank, mpisize),
-        b = mpi_seg_end  (0, n, mpirank, mpisize);
     dataType *data = new dataType[n];
 
     for(int k = 0; k < 15; ++k)
     {
-        #pragma omp parallel firstprivate(data)
-        {
-            srand(time(NULL) ^ omp_get_thread_num() ^ mpirank);
-            unsigned int seed = rand();
-            #pragma omp for
-            for(i = a; i < b; ++i)  {
-                data[i].key = (long long *)randull(&seed);
-            }
-        }
         failed = false;
         set<long long> ints;
 
         if(mpirank == 0)    {
-            for(i = 1; i < mpisize; ++i)    {
-                int start = mpi_seg_start(0, n, i, mpisize),
-                    end   = mpi_seg_end  (0, n, i, mpisize);
-                if(start < end)
-                    MPI_Recv(data+start, end-start, dataType_MPI(), i,
-                            MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            #pragma omp parallel firstprivate(data)
+            {
+                srand(time(NULL) ^ omp_get_thread_num() ^ mpirank);
+                unsigned int seed = rand();
+                #pragma omp for
+                for(i = 0; i < n; ++i)  {
+                    data[i].key = (long long *)randull(&seed);
+                }
             }
 
             for(i = 0; i < n; ++i)
                 ints.insert((long long)data[i].key);
         }
-        else if(a < b)   {
-            MPI_Send(data+a, b-a, dataType_MPI(), 0, 0, MPI_COMM_WORLD);
-        }
 
         MPI_Barrier(MPI_COMM_WORLD);
         pSort(data, n, MERGE);
-        MPI_Barrier(MPI_COMM_WORLD);
 
         if(mpirank == 0)    {
             for(i = 0; i < n-1; ++i)
@@ -78,6 +64,8 @@ int main(int argc, char **argv)  {
                 }
             }
 
+            MPI_Bcast(&failed, 1, MPI_BYTE, 0, MPI_COMM_WORLD);
+
             if(failed)  {
                 printf("Sort failed!\n");
                 #ifdef DEBUG
@@ -85,10 +73,17 @@ int main(int argc, char **argv)  {
                     printf("%lld\n", (long long)data[i].key);
                 #endif
                 MPI_Finalize();
-                return -1;
+                return 0;
             }
 
             printf("Sort succesful.\n");
+        }
+        else    {
+            MPI_Bcast(&failed, 1, MPI_BYTE, 0, MPI_COMM_WORLD);
+            if(failed)  {
+                MPI_Finalize();
+                return 0;
+            }
         }
     }
 
--- time.cpp
+++ time.cpp
@@ -7,41 +7,25 @@ using namespace std;
 
 int main(int argc, char **argv)  {
     MPI_Init(&argc, &argv);
-    MPI_Status status;
     int mpirank, mpisize;
     MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
     MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
 
-    const int n = 1<<25;
+    const int n = 1<<27;
     int i;
-    int a = mpi_seg_start(0, n, mpirank, mpisize),
-        b = mpi_seg_end  (0, n, mpirank, mpisize);
     dataType *data = new dataType[n];
     
-    #pragma omp parallel firstprivate(data)
-    {
-        srand(time(NULL) ^ omp_get_thread_num());
-        unsigned int seed = rand();
-        #pragma omp for
-        for(i = a; i < b; ++i)  {
-            data[i].key = (long long *)randull(&seed);
-        }
-    }
-
     if(mpirank == 0)    {
-        printf("Generated random numbers..\n");
-        for(i = 1; i < mpisize; ++i)    {
-            int start = mpi_seg_start(0, n, i, mpisize),
-                end   = mpi_seg_end  (0, n, i, mpisize);
-            if(start < end)
-                MPI_Recv(data+start, end-start, dataType_MPI(), i,
-                        MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        #pragma omp parallel firstprivate(data)
+        {
+            srand(time(NULL) ^ omp_get_thread_num());
+            unsigned int seed = rand();
+            #pragma omp for
+            for(i = 0; i < n; ++i)  {
+                data[i].key = (long long *)randull(&seed);
+            }
         }
-
-        printf("Collected data, beginning sorting..\n");
-    }
-    else if(a < b)   {
-        MPI_Send(data+a, b-a, dataType_MPI(), 0, 0, MPI_COMM_WORLD);
+        printf("Generated random numbers..\n");
     }
     
     double start, end;
@@ -50,7 +34,6 @@ int main(int argc, char **argv)  {
     pSort(data, n, MERGE);
     end = MPI_Wtime();
 
-    printf("done.\n");
     if(mpirank == 0)    {
         for(i = 0; i < n-1; ++i)
             if((long long)data[i].key > (long long)data[i+1].key) {
