6269f5255d1fd68a04a92aee41eb741ebe53eb64
Shivanker Goel <shivanker.goel@gmail.com>
2015-03-06 01:30:54 +0530

    lil improvement in msort code-wise

--- msort.cpp
+++ msort.cpp
@@ -113,29 +113,23 @@ void mSort(dataType *data, int n_total)    {
         if(mpirank % factor != 0) {
             // hand over data to parent, and deactivate
             int parent = mpirank - (mpirank % factor);
-            MPI_Send(&b, 1, MPI_INT, parent, factor+4096, MPI_COMM_WORLD);
-            if(a < b)   {
-                MPI_Send(data+a, b-a, dataType_MPI(), parent,
-                            factor, MPI_COMM_WORLD);
-            }
+            MPI_Send(data+a, b-a, dataType_MPI(), parent,
+                        factor, MPI_COMM_WORLD);
             active = false;
         }
         else if(mpirank+factor/2 < mpisize) {
             // get data from all child, and merge
-            int temp;
-            MPI_Recv(&temp, 1, MPI_INT, MPI_ANY_SOURCE,
-                        factor+4096, MPI_COMM_WORLD, &status);
-            if(b < temp)    {
-                MPI_Recv(data+b, temp-b, dataType_MPI(), MPI_ANY_SOURCE,
-                            factor, MPI_COMM_WORLD, &status);
-                merge(data+a, b-a, temp-a, res+a);
-                b = temp;
+            int recvd;
+            MPI_Recv(data+b, n, dataType_MPI(), MPI_ANY_SOURCE,
+                        factor, MPI_COMM_WORLD, &status);
+            MPI_Get_count(&status, dataType_MPI(), &recvd);
+            merge(data+a, b-a, recvd+b-a, res+a);
+            b += recvd;
 
-                // copy back from res to data
-                #pragma omp parallel for 
-                for(int i = a; i < b; ++i)
-                    data[i] = res[i];
-            }
+            // copy back from res to data
+            #pragma omp parallel for 
+            for(int i = a; i < b; ++i)
+                data[i] = res[i];
         }
     } while(active && divceil(mpisize, factor) > 1);
 
